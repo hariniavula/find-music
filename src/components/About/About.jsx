c8d449d5dad662fff5d1d9319761c4d4461805d8
hariniavula <avula.ha@northeastern.edu>
2024-08-11 19:02:18 -0500

    about page responsiveness

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,35 +2,57 @@ import React from 'react';
 import './About.css';
 
 
-  const Button = ({ link, children }) => {
-    const handleClick = () => {
-      window.location.href = link;
-    };
-  
-    return (
-      <button onClick={handleClick}>
-        {children}
-      </button>
-    );
+const Button = ({ link, children }) => {
+  const handleClick = () => {
+    window.location.href = link;
   };
 
+  return (
+    <button onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
+/* 
+const About = () => {
+  return (
+    
+    <div className="about">
+      <div className="about-left">
+        <img src="/src/assets/smaller-about.png" alt="harini" className="about-image" />
+      </div>
+      <div className="about-right">
+        <div className="about-title">
+          <h1>About Me</h1>
+        </div>
+        <div className="about-para">
+          <p>Hi! My name is Harini. I'm a sophomore studying computer science and economics at Northeastern University. My favorite types of music are folk, pop, and country! I created this site because I'm always on the lookout for up-and-coming artists.</p>
+          <p>If you would like to connect with me or learn more about my work and interests, please visit my website.</p>
+          <Button link="https://hariniavula.github.io/harini-website/">Visit My Website</Button>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default About;
+ */
 
 
-  const About = () => {
+const About = () => {
   return (
     <div className = "entire-about">
-    <div id = "about-me" className="about">
-      
+    <div id = "title" className="about">
       <div className="about-text">
-        <h3>About me</h3>
+        <h1>About Me</h1>
         <br/>
-        <p> Hi! My name is Harini. I'm a sophomore studying computer science and economics at Northeastern University. My favorite types of music are folk, pop, and country! I created this site because I'm always on the lookout for up-and-coming artists. </p>
-        <br/>
-        <p>If you would like to connect with me or learn more about my work and interests, please visit my website.</p>
-        <br/>
-        <Button link="https://hariniavula.github.io/harini-website/">Visit My Website</Button>
+        <p>Hi! My name is Harini. I'm a sophomore studying computer science and economics at Northeastern University. My favorite types of music are folk, pop, and country! I created this site because I'm always on the lookout for up-and-coming artists.</p>
+          <p>If you would like to connect with me or learn more about my work and interests, please visit my website.</p>
+          <Button link="https://hariniavula.github.io/harini-website/">Visit My Website</Button>
       </div>
-      <img src="/src/assets/smaller-about.png" alt="harini" className="about-image" />
+      <img src="/src/assets/smaller-about.png" alt="Harini Profile" className="about-image" />
+
     </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
         <h1> next favorite artist </h1>
         <br/>
         <p>Dive into a world of fresh sounds and undiscovered talent! Take our quick music quiz to identify your preferred genres, or start exploring!</p>
-        <br/>
         <p>By supporting these talented individuals, you'll not only expand your musical horizons but also help the artists grow and thrive. Discover, enjoy, and support the future of music today!</p>
       </div>
     </div>

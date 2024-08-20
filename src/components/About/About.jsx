19c8756ceeb54ac0f5b84c296dbd59c125eb4d14
hariniavula <avula.ha@northeastern.edu>
2024-08-20 17:47:57 -0500

    about image import

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './About.css';
+import about from "./assets/about.jpg"
+
 
 
 const Button = ({ link, children }) => {
@@ -13,31 +15,6 @@ const Button = ({ link, children }) => {
     </button>
   );
 };
-/* 
-const About = () => {
-  return (
-    
-    <div className="about">
-      <div className="about-left">
-        <img src="/src/assets/smaller-about.png" alt="harini" className="about-image" />
-      </div>
-      <div className="about-right">
-        <div className="about-title">
-          <h1>About Me</h1>
-        </div>
-        <div className="about-para">
-          <p>Hi! My name is Harini. I'm a sophomore studying computer science and economics at Northeastern University. My favorite types of music are folk, pop, and country! I created this site because I'm always on the lookout for up-and-coming artists.</p>
-          <p>If you would like to connect with me or learn more about my work and interests, please visit my website.</p>
-          <Button link="https://hariniavula.github.io/harini-website/">Visit My Website</Button>
-        </div>
-      
-      </div>
-    </div>
-  );
-}
-
-export default About;
- */
 
 
 const About = () => {
@@ -51,7 +28,7 @@ const About = () => {
           <p>If you would like to connect with me or learn more about my work and interests, please visit my website.</p>
           <Button link="https://hariniavula.github.io/harini-website/">Visit My Website</Button>
       </div>
-      <img src="/src/assets/smaller-about.png" alt="Harini Profile" className="about-image" />
+      <img src={about}alt="Harini Profile" className="about-image" />
 
     </div>
     </div>
